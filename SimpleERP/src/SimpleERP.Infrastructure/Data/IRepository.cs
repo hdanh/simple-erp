6aa214335ccf1833062044d7c10c582ab44ee95a
Danh Hoang <danh.hoang1991@gmail.com>
2016-09-28 01:16:31 +0700

    update

--- SimpleERP/src/SimpleERP.Infrastructure/Data/IRepository.cs
+++ SimpleERP/src/SimpleERP.Infrastructure/Data/IRepository.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+using SimpleERP.Infrastructure.Models;
 
 namespace SimpleERP.Infrastructure.Data
 {
-    interface IRepository
+    public interface IRepository<T, in TId>
+        where T : BaseEntity<TId>
     {
+        IQueryable<T> Query();
+
+        void Add(T entity);
+
+        IDbContextTransaction BeginTransaction();
+
+        void SaveChange();
+
+        void Remove(T entity);
     }
 }
--- SimpleERP/src/SimpleERP.Objects/Properties/AssemblyInfo.cs
+++ SimpleERP/src/SimpleERP.Objects/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleERP.Core")]
+[assembly: AssemblyProduct("SimpleERP.Objects")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("02df5af8-5e33-4cba-b964-6db07330ba63")]
+[assembly: Guid("5264e4f9-09be-45f7-8375-0b8ef6080d4b")]
